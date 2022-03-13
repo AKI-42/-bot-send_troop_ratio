06979e87796caf3f7d30353d7376776f730d8f79
IORI42 <iori.42.0001@gmail.com>
2022-03-14 02:29:58 +0900

    test

--- discordbot.py
+++ discordbot.py
@@ -48,21 +48,19 @@ async def test1(ctx, *args: int):
 		main_troop__ratio = 0.6
 	elif len(args) == 3:
 		main_troop_ratio = args[2] / 10
-	
-	sac_troop_ratio = 1 - main_troop__ratio
-	
+		
 	inf_heigh_t = math.floor((inf_ratio*uni)*main_troop__ratio)
-	inf_low_t = math.floor((inf_ratio*uni)*sac_troop_ratio)
-	
+	inf_low_t = (inf_ratio*uni) - inf_heigh_t
+
 	range_heigh_t = math.floor((range_ratio*uni)*main_troop__ratio)
-	range_low_t = math.floor((range_ratio*uni)*sac_troop_ratio)
+	range_low_t = (range_ratio*uni) - range_heigh_t
 	
-	cav_heigh_t = 1
-	cav_low_t = 1
+	cav_heigh_t = math.floor((cav_ratio*uni)*main_troop_ratio)
+	cav_low_t = (cav_ratio*uni) - cav_heigh_t
+
 	await ctx.send(f"歩兵  {inf_heigh_t:<10,}死役  {inf_low_t:<10,}  |  合計  {inf_heigh_t+inf_low_t:>10,}\n\
 弓兵  {range_heigh_t:<10,}死役  {range_low_t:<10,}  |  合計  {range_heigh_t+range_low_t:>10,}\n\
 騎兵  {cav_heigh_t:<10,}死役  {cav_low_t:<10,}  |  合計  {cav_heigh_t+cav_low_t:>10,}")
-	await ctx.send("test9")
 
 
 @bot.command()
