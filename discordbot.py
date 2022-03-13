af728e10f896ac8b72c88159635e47a40013cf6f
IORI42 <iori.42.0001@gmail.com>
2022-03-14 02:54:35 +0900

    embed_test

--- discordbot.py
+++ discordbot.py
@@ -1,6 +1,7 @@
 from discord.ext import commands
 from os import getenv
 import traceback
+import discord
 import math
 
 bot = commands.Bot(command_prefix='/')
@@ -31,6 +32,31 @@ async def ratio(ctx, main: int, ratio: int, max_size: int):
 歩兵  {inf_heigh_t:<10,}死役  {inf_low_t:<10,}  |  合計  {inf_heigh_t+inf_low_t:>10,}\n\
 弓兵  {range_heigh_t:<10,}死役  {range_low_t:<10,}  |  合計  {range_heigh_t+range_low_t:>10,}\n\
 騎兵  {cav_heigh_t:<10,}死役  {cav_low_t:<10,}  |  合計  {cav_heigh_t+cav_low_t:>10,}")
+
+@bot.command()
+async def embed_test(ctx):
+
+	embed = discord.Embed( # Embedを定義する
+    	title="Example Embed",# タイトル
+        color=0x00ff00, # フレーム色指定(今回は緑)
+        description="Example Embed for Advent Calendar", # Embedの説明文 必要に応じて
+        url="https://example.com" # これを設定すると、タイトルが指定URLへのリンクになる
+        )
+	embed.set_author(name=client.user, # Botのユーザー名
+                     url="https://repo.exapmle.com/bot", # titleのurlのようにnameをリンクにできる。botのWebサイトとかGithubとか
+                     icon_url=client.user.avatar_url # Botのアイコンを設定してみる
+                     )
+
+	embed.set_thumbnail(url="https://image.example.com/thumbnail.png") # サムネイルとして小さい画像を設定できる
+
+	embed.set_image(url="https://image.example.com/main.png") # 大きな画像タイルを設定できる
+
+	embed.add_field(name="フィールド１",value="値１") # フィールドを追加。
+	embed.add_field(name="フィールド２",value="値２")
+
+	embed.set_footer(text="made by nashiroaoi", # フッターには開発者の情報でも入れてみる
+                     icon_url="https://dev.exapmple.com/profile.png")
+	await ctx.send(embed=embed) # embedの送信には、embed={定義したembed名}
 
 token = getenv('DISCORD_BOT_TOKEN')
 bot.run(token)
