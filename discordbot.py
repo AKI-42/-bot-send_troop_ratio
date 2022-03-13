404dde1da4a6a806cbfc282dadd8aaad0d60aa2b
IORI42 <iori.42.0001@gmail.com>
2022-03-14 02:41:08 +0900

    arrange

--- discordbot.py
+++ discordbot.py
@@ -27,7 +27,7 @@ async def ratio(ctx, main: int, ratio: int, max_size: int):
 	cav_heigh_t = math.floor((cav_ratio*uni)*main)
 	cav_low_t = math.floor((cav_ratio*uni) - cav_heigh_t)
 
-	await ctx.send(f"死役比率 {10 - (main*10)}\n\
+	await ctx.send(f"死役 {math.floor(10 - (main*10))}割\n\
 歩兵  {inf_heigh_t:<10,}死役  {inf_low_t:<10,}  |  合計  {inf_heigh_t+inf_low_t:>10,}\n\
 弓兵  {range_heigh_t:<10,}死役  {range_low_t:<10,}  |  合計  {range_heigh_t+range_low_t:>10,}\n\
 騎兵  {cav_heigh_t:<10,}死役  {cav_low_t:<10,}  |  合計  {cav_heigh_t+cav_low_t:>10,}")
