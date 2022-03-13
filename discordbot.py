59db6609cb38b327c8ec9367c529b6d1751aa904
IORI42 <iori.42.0001@gmail.com>
2022-03-13 22:36:36 +0900

    add_new_func_chage_ratio

--- discordbot.py
+++ discordbot.py
@@ -29,5 +29,36 @@ async def ratio(ctx, ratio: int, max_size: int):
 弓兵  {range_heigh_t:<10,}死役  {range_low_t:<10,}  |  合計  {range_heigh_t+range_low_t:>10,}\n\
 騎兵  {cav_heigh_t:<10,}死役  {cav_low_t:<10,}  |  合計  {cav_heigh_t+cav_low_t:>10,}")
 
+@bot.command()
+async def test(ctx, *arg):
+
+	inf_ratio = math.floor(arg[0]/100)
+	arg[0] = math.floor(arg[0]%100)
+	range_ratio = math.floor(arg[0]/10)
+	cav_ratio = arg[0] % 10
+
+	total = inf_ratio + range_ratio + cav_ratio
+	uni = arg[1] / total
+
+	if len(arg) == 2:
+		main_troop__ratio = 0.6
+	elif len(arg) == 3:
+		main_troop_ratio = arg[2] / 10
+
+	sac_troop_ratio = 1 - main_troop__ratio
+
+	inf_heigh_t = math.floor((inf_ratio*uni)*main_troop__ratio)
+	inf_low_t = math.floor((inf_ratio*uni)*sac_troop_ratio)
+
+	range_heigh_t = math.floor((range_ratio*uni)*main_troop__ratio)
+	range_low_t = math.floor((range_ratio*uni)*sac_troop_ratio)
+
+	cav_heigh_t = math.floor((cav_ratio*uni)*main_troop_ratio)
+	cav_low_t = math.floor((cav_ratio*uni)*sac_troop_ratio)
+
+	await ctx.send(f"歩兵  {inf_heigh_t:<10,}死役  {inf_low_t:<10,}  |  合計  {inf_heigh_t+inf_low_t:>10,}\n\
+弓兵  {range_heigh_t:<10,}死役  {range_low_t:<10,}  |  合計  {range_heigh_t+range_low_t:>10,}\n\
+騎兵  {cav_heigh_t:<10,}死役  {cav_low_t:<10,}  |  合計  {cav_heigh_t+cav_low_t:>10,}")
+
 token = getenv('DISCORD_BOT_TOKEN')
 bot.run(token)
